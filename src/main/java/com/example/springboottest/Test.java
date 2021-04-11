2bc4fee56b07dda7a4d490bf699c0846d247251c
gys <gys1006@outlook.com>
2021-04-11 13:34:07 +0800

    测试

---
+++ src/main/java/com/example/springboottest/Test.java
@@ -0,0 +1,13 @@
+package com.example.springboottest;
+
+
+import com.sun.deploy.util.StringUtils;
+
+public class Test {
+
+    private Boolean test(String s){
+
+
+
+    }
+}
---
+++ src/main/java/com/example/springboottest/test/KafkaTest.java
@@ -0,0 +1,10 @@
+package com.example.springboottest.test;
+
+import org.apache.kafka.clients.KafkaClient;
+
+public class KafkaTest {
+    private KafkaClient kafkaClient;
+    public void tesggt(){
+        System.out.println("cesi");
+    }
+}
