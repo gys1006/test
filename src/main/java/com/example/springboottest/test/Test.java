fa24ff0a678ceae61e2f8017ba2258b27867a24b
gys <gys1006@outlook.com>
2021-08-03 18:05:02 +0800

    jenkins

--- src/main/java/com/example/springboottest/test/Test.java
+++ src/main/java/com/example/springboottest/test/Test.java
@@ -1,6 +1,5 @@
 package com.example.springboottest.test;
 
-import org.apache.kafka.clients.KafkaClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
