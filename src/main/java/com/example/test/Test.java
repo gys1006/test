27804c702a6234ac5d5946b59bf2f73ff877a1de
gys <gys1006@outlook.com>
2021-08-04 11:33:38 +0800

    jenkins

--- src/main/java/com/example/test/Test.java
+++ src/main/java/com/example/test/Test.java
@@ -1,4 +1,4 @@
-package com.example.springboottest.test;
+package com.example.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
